762da21ff6baa41e3f1f9ac155eb2e29f387b8c8
R. LaRoi <are@raewhoo.com>
2024-07-05 17:15:26 -0400

    Middleware

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 3000;
+const bodyParser = require("body-parser");
 
 app.set("view engine", "pug");
-app.set("views", "views");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  res.render("./index", {
-    title: "Pug",
-  });
+  res.render(
+    "./index",
+    {
+      title: "Pug",
+    },
+    { error: false }
+  );
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+router.get("/about", (req, res) => {
+  res.render("./about", { title: "Pug Two" });
 });
 
 app.listen(port, () => {
