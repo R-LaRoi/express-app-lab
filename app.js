1f220c4ff2489915c9d6b5b622c6f68304c29f6f
R. LaRoi <are@raewhoo.com>
2024-07-06 15:41:39 -0400

    Included fetch node and API

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
 const express = require("express");
-const app = express();
-const path = require("path");
 const port = 3000;
-const bodyParser = require("body-parser");
+const app = express();
+
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
 app.get("/", (req, res) => {
-  res.render("./index", {
+  res.render("index", {
     title: "Pug",
   });
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.get("/secondpg", function (req, res) {
-  const username = req.body.username;
-  let userLogin = login(username, req.body.password);
-  if (userLogin) {
-    res.render("/secondpg");
-  } else {
-    res.render("index", { error: true });
-  }
-});
+const url = `https://api.api-ninjas.com/v1/randomword`;
+const generateWord = {
+  method: "GET",
+  headers: {
+    "X-Api-Key": "4oizHQmbKbbQVNBZoHENP5tGR5mgpaIAucOaRp2T",
+  },
+};
+
+fetch(url, generateWord)
+  .then((res) => res.json())
+  .then((data) => {
+    let word = data.word.toString();
+    console.log(word);
+
+    app.get("/secondpg", function (req, res) {
+      res.render("secondpg", {
+        location: "Bali",
+        year: "2024",
+        word: word,
+        description:
+          "Bonbon cotton candy brownie sweet roll jelly beans shortbread. Lollipop brownie pastry cheesecake macaroon.",
+      });
+    });
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
